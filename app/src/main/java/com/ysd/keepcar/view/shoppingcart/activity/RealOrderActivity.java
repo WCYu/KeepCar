1690ea0f30eb5cccfa0b4de35d572716749d4f3f
Administrator <2137747786@qq.com>
2018-01-03 18:35:43 +0800

    提交关于购物车第一次

---
+++ app/src/main/java/com/ysd/keepcar/view/shoppingcart/activity/RealOrderActivity.java
@@ -0,0 +1,17 @@
+package com.ysd.keepcar.view.shoppingcart.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ysd.keepcar.R;
+/*确定订单
+*
+* */
+public class RealOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_real_order);
+    }
+}
