ebe6c629ed279073f6a9a569a4e67ad00ab80c19
阿禹 <13466941275@163.com>
2018-01-04 16:38:54 +0800

    王超禹修改

--- app/src/main/java/com/ysd/keepcar/utils/CustomPopuWindow.java
+++ app/src/main/java/com/ysd/keepcar/utils/CustomPopuWindow.java
@@ -10,18 +10,20 @@ import android.widget.PopupWindow;
  */
 
 public class CustomPopuWindow extends PopupWindow {
-    public CustomPopuWindow(View contentView, int width, int height){
-        super(contentView,width,height);
+    public CustomPopuWindow(View contentView, int width, int height) {
+        super(contentView, width, height);
     }
+
     /**
      * 在android7.0上，如果不主动约束PopuWindow的大小，比如，设置布局大小为 MATCH_PARENT,那么PopuWindow会变得尽可能大，
      * 以至于 view下方无空间完全显示PopuWindow，而且view又无法向上滚动，此时PopuWindow会主动上移位置，直到可以显示完全。
-     *　解决办法：主动约束PopuWindow的内容大小，重写showAsDropDown方法：
+     * 　解决办法：主动约束PopuWindow的内容大小，重写showAsDropDown方法：
+     *
      * @param anchor
      */
     @Override
     public void showAsDropDown(View anchor) {
-        if(Build.VERSION.SDK_INT == 24) {
+        if (Build.VERSION.SDK_INT == 24) {
             Rect rect = new Rect();
             anchor.getGlobalVisibleRect(rect);
             int h = anchor.getResources().getDisplayMetrics().heightPixels - rect.bottom;
@@ -32,7 +34,7 @@ public class CustomPopuWindow extends PopupWindow {
 
     @Override
     public void showAsDropDown(View anchor, int xoff, int yoff) {
-        if(Build.VERSION.SDK_INT == 24) {
+        if (Build.VERSION.SDK_INT == 24) {
             Rect rect = new Rect();
             anchor.getGlobalVisibleRect(rect);
             int h = anchor.getResources().getDisplayMetrics().heightPixels - rect.bottom;
